ef89eefd65e453e0e19a757efe0c5961905f737f
Aleksey Shishkin <barsuk-b@yandex.ru>
2022-07-03 17:39:56 +0300

    сделал кастомный селект категорий

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,9 +4,15 @@ import useWindowSize from '../hooks/useWindowSize'
 
 function Categories() {
 	const [selectedCategory, setSelectedCategory] = React.useState(0)
+	const [onOpenCategoryList, setOpenCategoryList] = React.useState(false)
 
 	const currentWindowWidth = useWindowSize()
 	const categoriesList = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+	const updateSelectedCategoryItem = index => {
+		setSelectedCategory(index)
+		setOpenCategoryList(false)
+	}
 
 	return (
 		<div className="categories">
@@ -22,13 +28,42 @@ function Categories() {
 					))}
 				</ul>
 			) : (
-				<select name="category" id="category">
-					{categoriesList.map((category, index) => (
-						<option key={index} value={category} onClick={() => setSelectedCategory(index)}>
-							{category}
-						</option>
-					))}
-				</select>
+				<div className="categories__select">
+					<div
+						className="categories__label"
+						onClick={() => setOpenCategoryList(!onOpenCategoryList)}>
+						<span>{categoriesList[selectedCategory]}</span>
+						<svg
+							className={onOpenCategoryList ? 'open' : ''}
+							width="15"
+							height="15"
+							viewBox="0 0 284.929 284.929"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg">
+							<path
+								d="M282.082,195.285L149.028,62.24c-1.901-1.903-4.088-2.856-6.562-2.856s-4.665,0.953-6.567,2.856L2.856,195.285
+								C0.95,197.191,0,199.378,0,201.853c0,2.474,0.953,4.664,2.856,6.566l14.272,14.271c1.903,1.903,4.093,2.854,6.567,2.854
+								c2.474,0,4.664-0.951,6.567-2.854l112.204-112.202l112.208,112.209c1.902,1.903,4.093,2.848,6.563,2.848
+								c2.478,0,4.668-0.951,6.57-2.848l14.274-14.277c1.902-1.902,2.847-4.093,2.847-6.566
+								C284.929,199.378,283.984,197.188,282.082,195.285z"
+								fill="#2C2C2C"
+							/>
+						</svg>
+					</div>
+
+					{onOpenCategoryList && (
+						<ul className="categories__popup">
+							{categoriesList.map((name, index) => (
+								<li
+									onClick={() => updateSelectedCategoryItem(index)}
+									key={index}
+									className={selectedCategory === index ? 'active' : ''}>
+									{name}
+								</li>
+							))}
+						</ul>
+					)}
+				</div>
 			)}
 		</div>
 	)
